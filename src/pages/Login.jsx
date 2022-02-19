87b0d1de5f4907750523820a935b8cfb9a1e1a2a
Luis Angel <angelfgdeveloper@gmail.com>
2022-02-19 16:01:56 -0600

    Modificacion en babel, alias en webpack & uso de imagenes, uso de hooks en componentes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/Login.scss';
 
+import logo from '@logos/logo_yard_sale.svg';
+
 const Login = () => {
+
+  const form = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // No redirige a otra página con los datos en la ruta
+
+    const formData = new FormData(form.current);
+    const data = {
+      email: formData.get('email'),
+      password: formData.get('password')
+    }
+
+    console.log(data);
+  }
+
+  // El id lo cambiamos a name para obtener la referencia
+  // <input type="email" id="email" placeholder="platzi@example.com" className="input input-email" />
+
   return (
     <div className="login">
       <div className="form-container">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-        <form action="/" className="form">
+        <img src={logo} alt="logo" className="logo" />
+        <form action="/" className="form" ref={form}>
           <label htmlFor="email" className="label">Email Address</label>
-          <input type="email" id="email" placeholder="platzi@example.com" className="input input-email" />
+          <input type="email" name="email" placeholder="platzi@example.com" className="input input-email" />
           <label htmlFor="password" className="label">Password</label>
-          <input type="password" id="password" placeholder="*********" className="input input-password" />
-          <input type="submit" defaultValue="Log in" className="primary-button login-button" />
+          <input type="password" name="password" placeholder="*********" className="input input-password" />
+          <button onClick={handleSubmit} className="primary-button login-button">Log in</button>
           <a href="/">Forgot my password</a>
         </form>
         <button className="secondary-button signup-button">Sign up</button>
--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,20 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/'
   },
+  mode: 'development',
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@containers': path.resolve(__dirname, 'src/containers/'),
+      '@context': path.resolve(__dirname, 'src/context/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+      '@logos': path.resolve(__dirname, 'src/assets/logos/')
+    }
   },
-  mode: 'development',
   module: {
     rules: [
       {
@@ -37,6 +47,10 @@ module.exports = {
           "css-loader",
           "sass-loader"
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+				type: 'asset',
       }
     ]
   },
